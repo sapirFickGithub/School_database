8aefb3538370ea788bd1739c751dd8285c24d2bb
HadassahStulman <116640702+HadassahStulman@users.noreply.github.com>
2024-05-26 03:56:48 +0300

    Delete Stage_1 directory

--- Stage_1/dropTables.sql
+++
@@ -1,6 +0,0 @@
-drop table student;
-drop table lesson;
-drop table class_;
-drop table teacher;
-drop table subject;
-drop table room;
--- Stage_1/selectAll.sql
+++
@@ -1,6 +0,0 @@
-select * from room;
-select * from class_;
-select * from teacher;
-select * from subject;
-select * from lesson;
-select * from student;
