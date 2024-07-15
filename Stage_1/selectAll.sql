fac4f2d1576dfd2fd1012fd67e1c38d0d2ed91d8
Hadassah Stulman <dasstlman@gmail.com>
2024-07-15 17:12:33 +0300

    removing unnecessary parts

--- Stage_1/selectAll.sql
+++ Stage_1/selectAll.sql
@@ -4,12 +4,3 @@ select * from teacher;
 select * from subject;
 select * from lesson;
 select * from student;
-
-select c.class_id, c.grade, count(student_id)
-from class_ c join student s on c.class_id = s.class_id
-group by c.class_id, c.grade
-order by c.grade
-
-select s.student_id, s.birth_date, c.grade
-from student s join class_ c on s.class_id = c.class_id
-where c.grade = 12
